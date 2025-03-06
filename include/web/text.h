a22b6e711ec6743afd8b74a07e21e13ac49e200a
milosz275 <miloszmaculewicz@gmail.com>
2025-03-06 01:09:49 +0100

    Add absolute text drawing

--- include/web/text.h
+++ include/web/text.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <GLES3/gl3.h>
+#include <glm/vec2.hpp>
 
 namespace web_ui
 {
@@ -15,14 +17,25 @@ namespace web_ui
         /**
          * @brief Draws text wrapping Emscripten.
          * @details Supply (x, y) coordinates, text style to draw it on text canvas.
-         * @param x Coordinate x in (-1.0f, 1.0f) domain
-         * @param y Coordinate y in (-1.0f, 1.0f) domain
+         * @param x Coordinate x in (-1.0f, 1.0f) domain, where (0, 0) is center
+         * @param y Coordinate y in (-1.0f, 1.0f) domain, where (0, 0) is center
          * @param text String text to be drawn
          * @param font String font style, e.g. "30px Arial"
          * @param fill_style String fill style, e.g. "white"
          */
-        static void draw_text(float x = -0.9f, float y = 0.9f, std::string text = "Web UI", std::string font = "30px Arial", std::string fill_style = "white");
+        static void draw_text(glm::vec2 coords = {-0.9f, 0.9f}, std::string text = "Web UI", std::string font = "30px serif", std::string fill_style = "white");
         
+        /**
+         * @brief Draws text wrapping Emscripten.
+         * @details Supply (x, y) coordinates, text style to draw it on text canvas.
+         * @param x Coordinate x absolute in window resolution, where (0, 0) is upper-left corner
+         * @param y Coordinate y absolute in window resolution, where (0, 0) is upper-left corner
+         * @param text String text to be drawn
+         * @param font String font style, e.g. "30px Arial"
+         * @param fill_style String fill style, e.g. "white"
+         */
+        static void draw_text_absolute(glm::vec2 coords = {10.0f, 10.0f}, std::string text = "Web UI", std::string font = "30px serif", std::string fill_style = "white");
+
         /**
          * @brief Clears text canvas.
          * @details Prevents overlapping of printed text.
--- src/text.cpp
+++ src/text.cpp
@@ -62,12 +62,22 @@ namespace web_ui
         em_setup_canvas();
     }
 
-    void text::draw_text(float x, float y, std::string text, std::string font, std::string fill_style)
+    void text::draw_text(glm::vec2 coords, std::string text, std::string font, std::string fill_style)
     {
         int canvas_width = EM_ASM_INT({ return getCanvasSize().width; }, 0);
         int canvas_height = EM_ASM_INT({ return getCanvasSize().height; }, 0);
-        float pixel_x = (x + 1.0f) * 0.5f * canvas_width;
-        float pixel_y = (1.0f - y) * 0.5f * canvas_height;
+        float pixel_x = (coords.x + 1.0f) * 0.5f * canvas_width;
+        float pixel_y = (1.0f - coords.y) * 0.5f * canvas_height;
+        em_render_text(pixel_x, pixel_y, text.c_str(), font.c_str(), fill_style.c_str());
+    }
+
+    void text::draw_text_absolute(glm::vec2 coords, std::string text, std::string font, std::string fill_style)
+    {
+        float pixel_x = coords.x;
+        float pixel_y = coords.y;
+        // [ ] Load actual size instead of 10-99 formatted size (e.g. "12px Arial" but not "2px Arial")
+        if (isdigit(font[0]) && isdigit(font[1]))
+            pixel_y += std::stoi(font.substr(0, 2));
         em_render_text(pixel_x, pixel_y, text.c_str(), font.c_str(), fill_style.c_str());
     }
 

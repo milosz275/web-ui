2e5e98501508b91e99c2345c5b81bc8e2aff2683
milosz275 <miloszmaculewicz@gmail.com>
2025-03-24 22:31:15 +0100

    Add darkmode

--- include/web/background.h
+++ include/web/background.h
@@ -4,7 +4,20 @@ namespace web_ui
 {
     class background
     {
+    private:
+        static int darkmode;
+
     public:
+        /**
+         * Toggles and returns new darkmode state, can be either true or false.
+         */
+        static bool toggle_darkmode();
+
+        /**
+         * Returns new darkmode state, can be either true or false.
+         */
+        static bool get_darkmode();
+
         /**
          * Draw static background, gray on default
          */
--- src/background.cpp
+++ src/background.cpp
@@ -1,12 +1,40 @@
 #include "background.h"
 
+#include <emscripten.h>
+#include <emscripten/html5.h>
 #include <GLES3/gl3.h>
 
 namespace web_ui
 {
+    int background::darkmode;
+
+    bool background::toggle_darkmode()
+    {
+        int result = {EM_ASM_INT({ return toggleDarkmode(); }, 0)};
+        if (result)
+            return true;
+        else
+            return false;
+    }
+
+    bool background::get_darkmode()
+    {
+        int result = {EM_ASM_INT({ return getDarkmode(); }, 0)};
+        if (result)
+            return true;
+        else
+            return false;
+    }
+
     void background::draw_background()
     {
-        glClearColor(0.7f, 0.7f, 0.7f, 1.0f); // background
+        darkmode = {EM_ASM_INT({ return getDarkmode(); }, 0)};
+
+        if (!darkmode)
+            glClearColor(0.65f, 0.65f, 0.65f, 1.0f);
+        else
+            glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+
         glClear(GL_COLOR_BUFFER_BIT);
     }
 }
